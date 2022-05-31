9f7e117f19954778c2533cc769a2f5873504bb58
dipanwitasaha7009 <dipanwitasaha7009@gmail.com>
2022-05-31 20:36:27 +0600

    1st lab task

---
+++ 20-41854-1_Lab1/Lab1/Controllers/CVController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Wellcome to your Profile";
+            return View();
+        }
+        public ActionResult Person()
+        {
+            ViewBag.Message = "Personal Background";
+
+            return View();
+        }
+        public ActionResult Project()
+        {
+            ViewBag.Message = "Previous Projects";
+
+            return View();
+        }
+        public ActionResult Edu()
+        {
+            ViewBag.Message = "Educational Level";
+
+            return View();
+        }
+        public ActionResult Ref()
+        {
+            ViewBag.Message = "References";
+
+            return View();
+        }
+    }
+}
